1175717f32904687e1cfcda8ca2ee75bbac01553
pritesh <priteshmalviya32@gmail.com>
2023-10-11 17:59:32 +0530

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'eschool-abf76',
+    appId: '1:391936371053:web:2072717bed57ecaabe9264',
+    storageBucket: 'eschool-abf76.appspot.com',
+    apiKey: 'AIzaSyA4H6ox74rXsAXt8WJfXb4kaPK6zUVfiBg',
+    authDomain: 'eschool-abf76.firebaseapp.com',
+    messagingSenderId: '391936371053',
+  },
+};
